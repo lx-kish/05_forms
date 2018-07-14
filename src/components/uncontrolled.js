21ed9292e8cd97cac5ce311d5b2a9d16a438f633
lx kish <urban.ktulhu@gmail.com>
2018-07-14 23:27:36 +1200

    64 Controlled and Uncontrolled

--- src/components/uncontrolled.js
+++ src/components/uncontrolled.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 
 class Uncontrolled extends Component {
 
-    state = {
+    handleSubmitClick = (event) => {
+        event.preventDefault();
 
+        const values = {
+            name: this.name.value,
+            lastname: this.lastname.value
+        }
+        console.log(values)
     }
 
-    render(){
-        return(
-            <div>
-                Uncontrolled
+    render() {
+        return (
+            <div className="container">
+                <form>
+                    <div className="form_element">
+                        <label>Enter name</label>
+                        <input
+                            type="text"
+                            ref={input => this.name = input}
+                        />
+                    </div>
+                    <div className="form_element">
+                        <label>Enter lastname</label>
+                        <input
+                            type="text"
+                            ref={input => this.lastname = input}
+                        />
+                    </div>
+                    <button onClick={this.handleSubmitClick}> Sign in </button>
+                </form>
             </div>
         )
     }
